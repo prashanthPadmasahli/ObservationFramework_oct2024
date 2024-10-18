3ead394fdc6b19b10fb83aa37e4bb060c2a5d328
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-10-18 10:41:59 +0530

    Initial Commit

---
+++ ObservationFramework_oct2024/ObservationFramework_oct2024App.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationFramework_oct2024App.swift
+//  ObservationFramework_oct2024
+//
+//  Created by mac on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservationFramework_oct2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
